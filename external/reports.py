cfce7c30a948245cec194550364ee6df391b4b23
andreamalagarriga-jpg <andreamalagarriga@gmail.com>
2026-05-12 19:58:07 +0200

    Update reports.py

--- external/reports.py
+++ external/reports.py
@@ -5,27 +5,25 @@ from data.followups_repository import fetch_all_followups
 
 def export_followups_csv():
     """
-    Exporta CSV a /tmp.
-
-    Returns:
-        str
+    Exporta CSV temporal.
     """
 
     try:
         followups = fetch_all_followups()
 
+        if not followups:
+            return False, "No existen seguimientos para exportar."
+
         dataframe = pd.DataFrame(followups)
 
-        file_path = "/tmp/followups.csv"
+        file_path = "/tmp/petto_followups.csv"
 
         dataframe.to_csv(
             file_path,
             index=False
         )
 
-        return file_path
+        return True, file_path
 
     except Exception as error:
-        raise RuntimeError(
-            f"Error exportando CSV: {error}"
-        )
+        return False, f"Error exportando CSV: {error}"

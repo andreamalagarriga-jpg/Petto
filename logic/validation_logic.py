32e8017fd60f251702446d09a308704ddb2ea919
andreamalagarriga-jpg <andreamalagarriga@gmail.com>
2026-05-12 19:56:38 +0200

    Update validation_logic.py

--- logic/validation_logic.py
+++ logic/validation_logic.py
@@ -1,16 +1,23 @@
 def validate_required_text(value, field_name):
     """
     Valida texto requerido.
-
-    Inputs:
-        value (str)
-        field_name (str)
-
-    Returns:
-        tuple(bool, str)
     """
 
-    if not value or not value.strip():
-        return False, f"{field_name} es obligatorio"
+    if not value:
+        return False, f"{field_name} es obligatorio."
+
+    if not value.strip():
+        return False, f"{field_name} no puede estar vacío."
+
+    return True, ""
+
+
+def validate_positive_number(value, field_name):
+    """
+    Valida números positivos.
+    """
+
+    if value < 0:
+        return False, f"{field_name} debe ser positivo."
 
     return True, ""
